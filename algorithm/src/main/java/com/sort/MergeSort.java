98cb78ce704612d3e58e1ab4937e679c9fe1243c
dcsec.xlj <767248371@qq.com>
2019-02-18 15:39:25 +0800

    aaaa

--- algorithm/src/main/java/com/sort/MergeSort.java
+++ algorithm/src/main/java/com/sort/MergeSort.java
@@ -1,7 +1,14 @@
 package com.sort;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
+/**
+ * 归并排序
+ */
+@SuppressWarnings("ALL")
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
@@ -9,41 +16,41 @@ public class MergeSort {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void sort(int[] arr) {
-        int[] temp = new int[arr.length];//在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
-        sort(arr, 0, arr.length - 1, temp);
+    private static void sort(int[] arr) {
+        int[] tmp = new int[arr.length];
+        sort(arr, 0, arr.length - 1, tmp);
     }
 
-    private static void sort(int[] arr, int left, int right, int[] temp) {
+    private static void sort(int[] arr, int left, int right, int[] tmp) {
         if (left < right) {
-            int mid = (left + right) / 2;
-            sort(arr, left, mid, temp);//左边归并排序，使得左子序列有序
-            sort(arr, mid + 1, right, temp);//右边归并排序，使得右子序列有序
-            merge(arr, left, mid, right, temp);//将两个有序子数组合并操作
+            int middle = (left + right) >> 1;
+            sort(arr, left, middle, tmp);
+            sort(arr, middle + 1, right, tmp);
+            merge(arr, left, middle, right, tmp);
         }
     }
 
-    private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
-        int i = left;//左序列指针
-        int j = mid + 1;//右序列指针
-        int t = 0;//临时数组指针
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp[t++] = arr[i++];
+    private static void merge(int[] array, int lo, int mid, int hi, int[] temp) {
+        int left = lo;
+        int right = mid + 1;
+        int k = lo;
+        while (left <= mid && right <= hi) {
+            if (array[left] < array[right]) {
+                temp[k++] = array[left++];
             } else {
-                temp[t++] = arr[j++];
+                temp[k++] = array[right++];
             }
         }
-        while (i <= mid) {//将左边剩余元素填充进temp中
-            temp[t++] = arr[i++];
+        //处理剩余未合并的部分
+        while (left <= mid) {
+            temp[k++] = array[left++];
         }
-        while (j <= right) {//将右序列剩余元素填充进temp中
-            temp[t++] = arr[j++];
+        while (right <= hi) {
+            temp[k++] = array[right++];
         }
-        t = 0;
-        //将temp中的元素全部拷贝到原数组中
-        while (left <= right) {
-            arr[left++] = temp[t++];
+        //将临时数组中的内容存储到原数组中
+        while (lo <= hi) {
+            array[lo] = temp[lo++];
         }
     }
 }
